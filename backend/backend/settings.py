48497aff90e425f1114af3b9eea9902b50a7fc38
Thanaphat Pethdongjan <kokou102@hotmail.com>
2022-03-27 16:57:33 +0700

    up token 1 to 60

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -162,7 +162,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "treecher.NewUser"
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,

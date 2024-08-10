b07a5f503457867d55196027361d79c11f8db191
Thanaphat Pethdongjan <kokou102@hotmail.com>
2024-08-11 01:38:04 +0700

    update react prod config file

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -2,7 +2,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 
 const config = {
     isProd,
-    apiUrlPrefix: isProd ? '/api' : 'http://localhost:8000/api/'
+    apiUrlPrefix: isProd ? '/api' : 'http://dj_treecher_web_server:8000/api/'
 }
 
 export default config

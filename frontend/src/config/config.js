471e1a2cfce6e4f1d49f76e60bceb02e6276412a
Thanaphat Pethdongjan <kokou102@hotmail.com>
2024-08-12 17:51:36 +0700

    update prod file

--- frontend/src/config/config.js
+++ frontend/src/config/config.js
@@ -3,7 +3,7 @@ const isProd = !(!process.env.NODE_ENV || process.env.NODE_ENV === 'development'
 const config = {
     isProd,
     // apiUrlPrefix: isProd ? '/api' : 'http://dj_treecher_web_server:8000/api/'
-    apiUrlPrefix: isProd ? 'http://dj_treecher_web_server:8000/api/' : '/api'
+    apiUrlPrefix: isProd ? 'http://192.168.10.10:8000/api/' : '/api'
 }
 
 export default config
